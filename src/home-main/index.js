178fe809ac93625097b9bda244139960b4731b49
tostasqb <tostasqb@gmail.com>
2018-10-21 11:48:57 +0100

    Add hero to main component

--- src/home-main/index.js
+++ src/home-main/index.js
@@ -5,16 +5,22 @@ import './main.scss';
 class HomeMain extends Component {
   render() {
     return (
-      <section className="hl-main">
-        <div className="container">
-          <h1>Decore a sua casa com gosto</h1>
-          <h2>fale connosco, nós ajudamos</h2>
-          <div className="hl-main__actions">
-            <a href="#" className="hl-main__primary">Contacte-nos</a> 
-            <span>ou</span>
-            <a href="#" className="hl-main__secondary">Ver produtos</a>
+      <section class="hl-main hero is-large">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              Decore a sua casa com gosto
+            </h1>
+            <h2 class="subtitle">
+              fale connosco, nós ajudamos
+            </h2>
+            <div className="hl-main__actions">
+              <a href="#" className="hl-main__primary">Contacte-nos</a> 
+              <span>ou</span>
+              <a href="#" className="hl-main__secondary">Ver produtos</a>
+            </div>
+            <h3>Venda só a profissionais</h3>
           </div>
-          <h3>Venda só a profissionais</h3>
         </div>
       </section>
     );
