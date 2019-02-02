203912ecf98e3ae6ef597570028de74fde6a2d5c
tostasqb <tostasqb@gmail.com>
2019-02-02 16:56:46 +0000

    Footer responsiveness

--- src/footer/index.js
+++ src/footer/index.js
@@ -3,18 +3,22 @@ import './main.scss';
 
 class Footer extends Component {
   render() {
+    var currYear = new Date().getFullYear();
+
     return (
-      <section className="hl-footer">
+      <footer className="hl-footer">
         <div className="container">
           <ul className="">
-            <li>Facebook</li>
-            <li>Contactos</li>
-            <li>Termos e Condições</li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Contactos</a></li>
+            <li><a href="#">Termos e Condições</a></li>
           </ul>
-
-          <span className="hl-copyright">Todos os direitos reservados © 2018 High Line</span>
+          
+          <span className="hl-copyright">
+            Todos os direitos reservados © {currYear} High Line
+          </span>
         </div>
-      </section>
+      </footer>
     );
   }
 }
