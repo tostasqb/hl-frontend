3f8a20e62d8534fdb729540c3af0c574b7040a2e
tostasqb <tostasqb@gmail.com>
2019-02-03 23:22:45 +0000

    Test API fetch

--- src/home-products/index.js
+++ src/home-products/index.js
@@ -2,29 +2,72 @@ import React, { Component } from 'react';
 import './main.scss';
 
 class HomeProducts extends Component {
-  callApi() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      products: []
+    };
+  }
+
+  componentDidMount() {
     let endpoint = process.env.REACT_APP_API_URL + 'products';
+
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            products: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  renderProduct(product) {
+    return(
+      <li key={`product-${product.id}`}>
+        {product.title}
+      </li>
+    )
   }
 
   render() {
-    return (
-      <section className="hl-home-products">
-        <div className="container">
-          <header className="hl-section-header">
-            <h3 className="hl-title">Produtos</h3>
-            <h4 className="hl-subtitle">Criamos e escolhemos marcas de confiança</h4>
-          </header>
-          <div>
-            {this.callApi()}
-            aaa
-          </div>
+    const { error, isLoaded, products } = this.state;
 
-          <div className="hl-action">
-            <a href="#" className="hl-button">Ver produtos</a>
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <section className="hl-home-products">
+          <div className="container">
+            <header className="hl-section-header">
+              <h3 className="hl-title">Produtos</h3>
+              <h4 className="hl-subtitle">Criamos e escolhemos marcas de confiança</h4>
+            </header>
+            <div>
+              {products.map(product => (
+                this.renderProduct(product)
+              ))}
+            </div>
+
+            <div className="hl-action">
+              <a href="#" className="hl-button">Ver produtos</a>
+            </div>
           </div>
-        </div>
-      </section>
-    );
+        </section>
+      );
+    }
   }
 }
 
