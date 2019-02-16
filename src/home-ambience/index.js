2ef5efdced59d36054dcf73c67a046f490456d9c
tostasqb <tostasqb@gmail.com>
2019-02-16 23:14:38 +0000

    Add ambiences to homepage

--- src/home-ambience/index.js
+++ src/home-ambience/index.js
@@ -1,26 +1,77 @@
 import React, { Component } from 'react';
 import './main.scss';
+import axios from 'axios';
 
 class HomeAmbience extends Component {
-  render() {
-    return (
-      <section className="hl-home-ambience">
-        <div className="container">
-          <header className="hl-section-header">
-            <h3 className="hl-title">Ambientes</h3>
-            <h4 className="hl-subtitle">Porque é mais fácil ver as coisas num contexto</h4>
-          </header>
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      ambiences: []
+    };
+  }
 
-          <div>
-            aaa
-          </div>
+  componentDidMount() {
+    let endpoint = process.env.REACT_APP_API_URL + 'ambiences';
 
-          <div className="hl-action">
-            <a href="#" className="hl-button">Ver ambientes</a>
-          </div>
+    axios.get(endpoint)
+      .then(res => {
+        this.setState({ 
+          isLoaded: true, 
+          ambiences: res.data
+        })
+      }).catch(error => {
+        this.setState({ 
+          isLoaded: true, 
+          error
+        })
+      });
+  }
+
+  renderAmbience(ambience) {
+    const imageStyle = {
+      backgroundImage: `url(https://placeimg.com/640/480/nature)`
+    }
+
+    return(
+      <a href="#" className="column is-one-quarter-desktop" key={`ambience-${ambience.id}`}>
+        <div className="hl-ambience">
+          <div className="hl-ambience-image" style={imageStyle}></div>
         </div>
-      </section>
-    );
+      </a>
+    )
+  }
+
+  render() {
+    const { error, isLoaded, ambiences } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <section className="hl-home-ambience">
+          <div className="container">
+            <header className="hl-section-header">
+              <h3 className="hl-title">Ambientes</h3>
+              <h4 className="hl-subtitle">Porque é mais fácil ver as coisas num contexto</h4>
+            </header>
+
+            <div className="columns">
+              {ambiences.map(ambience => (
+                this.renderAmbience(ambience)
+              ))}
+            </div>
+
+            <div className="hl-action">
+              <a href="#" className="hl-button">Ver ambientes</a>
+            </div>
+          </div>
+        </section>
+      );
+    }
   }
 }
 
