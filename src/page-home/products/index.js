231131cecffd87003585d1bf1fd2025ef2108077
tostasqb <tostasqb@gmail.com>
2019-04-16 20:42:21 +0100

    Add link to products on homepage

--- src/page-home/products/index.js
+++ src/page-home/products/index.js
@@ -13,6 +13,15 @@ class HomeProducts extends Component {
     };
   }
 
+  slugify(text) {
+    return text.toString().toLowerCase()
+      .replace(/\s+/g, '-')           // Replace spaces with -
+      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+      .replace(/^-+/, '')             // Trim - from start of text
+      .replace(/-+$/, '');            // Trim - from end of text
+  }
+
   componentDidMount() {
     let endpoint = process.env.REACT_APP_API_URL + 'products';
 
@@ -36,7 +45,7 @@ class HomeProducts extends Component {
     }
 
     return(
-      <a href="#" className="column is-one-third-desktop" key={`product-${product.id}`}>
+      <Link to={`/product/${product.id}/${this.slugify(product.title)}`} className="column is-one-third-desktop" key={`product-${product.id}`}>
         <div className="hl-product">
           <div className="hl-product-image" style={imageStyle}></div>
           <div className="hl-product-top">
@@ -47,7 +56,7 @@ class HomeProducts extends Component {
             {product.description}
           </div>
         </div>
-      </a>
+      </Link>
     )
   }
 
