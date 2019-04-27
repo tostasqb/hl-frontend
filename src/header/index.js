2c8d95e551038ca7c87df05ea731076c00414421
tostasqb <tostasqb@gmail.com>
2019-04-27 16:25:50 +0100

    Mobile optimization

--- src/header/index.js
+++ src/header/index.js
@@ -70,7 +70,6 @@ class Header extends Component {
             // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
             el.classList.toggle('is-active');
             $target.classList.toggle('is-active');
-    
           });
         });
       }
