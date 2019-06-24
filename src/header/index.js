58f44a476aa34c6da68349e5797b50b99923f566
tostasqb <tostasqb@gmail.com>
2019-06-24 21:35:29 +0100

    Add rasch link

--- src/header/index.js
+++ src/header/index.js
@@ -38,6 +38,7 @@ class Header extends Component {
               <Link to="/ambiences" className="navbar-item" data-anchor="ambiences">
                 ambientes
               </Link>
+              <a className="navbar-item" href="https://walldesigner.rasch.de/gallery.do" rel="noopener noreferrer" target="_blank">rasch</a>
               <Link to="/about" className="navbar-item" data-anchor="about">
                 sobre nós
               </Link>
@@ -55,18 +56,18 @@ class Header extends Component {
     document.addEventListener('DOMContentLoaded', () => {
       // Get all "navbar-burger" elements
       const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    
+
       // Check if there are any navbar burgers
       if ($navbarBurgers.length > 0) {
-    
+
         // Add a click event on each of them
         $navbarBurgers.forEach( el => {
           el.addEventListener('click', () => {
-    
+
             // Get the target from the "data-target" attribute
             const target = el.dataset.target;
             const $target = document.getElementById(target);
-    
+
             // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
             el.classList.toggle('is-active');
             $target.classList.toggle('is-active');
